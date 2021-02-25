82ff8fb980971bdb69eb94420fa271210dc430d3
Andrew Templeton <andrewtempleton@MacBook-Pro.lan>
2021-02-24 18:50:34 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+
+# HEB Cheater
+
+### Requirements
+
+`nodejs >= 12`  
+Optional: If using `--open true` for magic tab opening, a system with `open` shell command with a valid browser configured to delegate `https://`-prefixed string arguments to said browser. macOS does this out of the box with Safari (or whatever your Finder / System Preferences uses for default browser settings).
+
+### Setup
+
+```
+git clone https://github.com/andrew-templeton/heb-cheater;
+cd heb-cheater;
+npm install;
+node index;
+```
+
+### Use
+
+`node index [all the args]`  
+
+All arguments used like: `--arg value`.  
+Boolean flags should be used as value of `true`, all default to `false`.
+
+###### `latitude`
+
+Floating point, signed latitude of the centerpoint of search.  
+Default: `-97.7437001`
+
+###### `longitude`
+
+Floating point, signed longitude of the centerpoint of search.  
+Default: `30.270077`
+
+###### `distance`
+
+Floating point, unsigned distance as the crow flies in miles from centerpoint you're willing to travel.  
+Default: `100`
+
+###### `minimum`
+
+Unsigned, `1`-is-minimum, integer for lowest number of available appointments needed at a store to qualify for selection within your radius. This is useful, because lots of stores with `1` appointment are noisy, and very difficult for you to instantly grab the sole available slot. If you set this to something like `10` or `20`, it will be a lot easier to react to the listing and book one of the slots before they are all claimed.  
+Default: `1`
+
+###### `interval`
+
+Unsigned, floating point value for the number of seconds to wait between pings when using `--watch true`. Recommend using no lower than `10`, as you could be blocked otherwise. You might have good results in a mass booking workflow if you use all of `--watch true --forever true --open true` and set this number to `60` (one minute), meaning every one minute it will open all viable stores with enough slots in your radius, allowing you to react en-masse in realtime to availability. Lower numbers in this use case would overwhelm the senses.  
+Default: `10`
+
+
+###### `watch`
+
+Boolean flag for if the system should keep checking until a store meeting your needs is found, versus trying one time and stopping.  
+
+###### `open`
+
+Boolean flag for if the system call to `open <found store urls>` should be used when stores are found. This will open LOTS of tabs if used with `--watch true --forever true`, take care.
+
+###### `forever`
+
+Boolean flag, when used with `--watch true`, which will cause the system to keep pinging for stores even after one or more valid stores meeting your conditions are found. Be careful using all three of the flags: `--watch true --forever true --open true`, as this will keep opening tabs for found stores every `--interval true`.
---
+++ index.js
@@ -0,0 +1,65 @@
+const Axios = require('axios')
+const { exec } = require('child_process')
+const Exec = require('util').promisify(exec)
+
+const LOCATION_LIST = 'https://heb-ecom-covid-vaccine.hebdigital-prd.com/vaccine_locations.json'
+
+const spheric = (a, b) => {
+	const R = 6371
+	const kmToMiFactor = 0.621371
+	const degToRadianFactor = 0.0174533
+  const dLat = (b.latitude - a.latitude) * degToRadianFactor
+  const dLong = (b.longitude - a.longitude) * degToRadianFactor
+  const z = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(a.latitude * degToRadianFactor) * Math.cos(b.latitude * degToRadianFactor) *
+      Math.sin(dLong / 2) * Math.sin(dLong / 2)
+  const c = 2 * Math.atan2(Math.sqrt(z), Math.sqrt(1 - z))
+  const d = R * c
+  return d * kmToMiFactor
+}
+
+const logJson = obj => console.log(JSON.stringify(obj, null, 2))
+const sortAsc = field => (a, b) => a[field] - b[field]
+const getLocations = async () => (await Axios.get(LOCATION_LIST)).data.locations.filter(({ openAppointmentSlots:o }) => !!o)
+const getLocationsNear = async ({ latitude, longitude, distance, minimum }) => (await getLocations())
+                                                                        .map(store => ({ ...store, distance: spheric(store, { latitude, longitude }) }))
+                                                                        .filter(store => store.distance <= distance && store.openAppointmentSlots >= minimum)
+                                                                        .sort(sortAsc('distance'))
+const openInBrowser = async store => { await Exec(`open '${store.url}'`); return store; }
+const openLocationsNear = async circle => await Promise.all((await getLocationsNear(circle)).map(openInBrowser))
+
+const any = result => result.length
+const wait = async time => await new Promise(resolve => setTimeout(resolve, time))
+const waitFor = ({ functor, finish, interval }) => async (...args) => {
+  while (true) {
+    const result = await functor(...args)
+    if (finish(result)) {
+      return result
+    }
+    await wait(interval * 1000)
+  }
+}
+
+const main = async ({ latitude, longitude, distance, watch, open, forever, minimum, interval }) => {
+  const functor = open ? openLocationsNear : getLocationsNear
+  const finish = forever ? console.log : any
+  const control = watch ? waitFor({ functor, finish, interval }) : functor
+  return await control({ latitude, longitude, distance, minimum })
+}
+
+
+if (!module.parent) {
+  var argv = require('yargs/yargs')(process.argv.slice(2))
+    .default('longitude', -97.7437001)
+    .default('latitude', 30.270077)
+    .default('distance', 100)
+    .default('watch', false)
+    .default('open', false)
+    .default('forever', false)
+    .default('minimum', 1)
+    .default('interval', 10)
+    .argv
+
+  const { latitude, longitude, distance, watch, open, forever, minimum, interval } = argv
+  main({ latitude, longitude, distance, watch, open, forever, minimum, interval }).then(logJson)
+}
